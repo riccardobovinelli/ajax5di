ef956ce5b6c24253c8c9b0abf035a7cd1e37adf8
edo01 <edocarra012@gmail.com>
2020-04-25 18:18:17 +0200

    add databaseExchange

---
+++ databaseExchange/body.php
@@ -0,0 +1,18 @@
+<?php
+$id = $_GET["id"];
+if($id == 1){
+    echo "Home";
+}
+else if($id == 2){
+    echo "About";
+}
+else if($id == 3){
+    echo "Portfolio";
+}
+else if($id == 4){
+    echo "Blog";
+}
+else if($id == 5){
+    echo "Contact";
+}
+?>
